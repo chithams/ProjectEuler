c2f5a5e606275348abacc9723c8f06fb055edc95
Ranjini <sranjinic@hotmail.com>
2014-08-24 10:45:05 +0200

    first level projecteuler

---
+++ projecteuler1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteuler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int solution1 = level1(1000, 3, 5);
+            Console.Out.WriteLine("The answer is: " + solution1);
+            Console.ReadKey();
+        }
+
+        static int level1(int below, int m1, int m2)
+        {
+            int result = 0;
+            for (int i = 1; i < below; i++)
+            {
+                if (i % m1 == 0 || i % m2 == 0)
+                    result = result + i;
+            }
+
+            return result;
+        }
+    }
+
+}
